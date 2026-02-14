2233c54ac86d9df20a1fa00b4a6923ccbb41bd3d
ShadowHipe <gn375294@gmail.com>
2026-02-14 13:17:45 -0300

    Update Doorshub.lua

--- Doorshub.lua
+++ Doorshub.lua
@@ -77,7 +77,7 @@ _G.ApexDoors = {
 
 -- [[ LOAD APEX LIB ]]
 local Success, ApexLib = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/joaorqqq/ApexLib/main/ApexLib.lua"))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/joaorqqq/ApexLib/refs/heads/main/ApexLib.lua"))()
 end)
 
 if not Success then 
